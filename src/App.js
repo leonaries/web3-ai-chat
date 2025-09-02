121fefbf2975418c4d358b9145b630062735ae05
leonaries <35209061+leonaries@users.noreply.github.com>
2025-09-03 00:20:34 +0800

    ⚛️ Add main App component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Web3AIChat from './components/Web3AIChat';
+
+function App() {
+  return (
+    <div className="App">
+      <Web3AIChat />
+    </div>
+  );
+}
+
+export default App;
